b984a72e7763bace83a4841c923b807ce8c23302
Rahul Arora <rahularora@rahuls-mbp-4.dyndns.rice.edu>
2023-10-28 15:10:20 -0500

    update 1

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import './App.css';
+import Servery from "./components/Servery"
+function App() {
+
+  const serveries = [
+    {
+      "name": "North Servery",
+      "review": "9/10",
+      "image": "https://kwtexas.com/wp-content/uploads/2015/01/Rice-Brown-College.jpg",
+      "menu":"https://dining.rice.edu/north-servery",
+      "text": ["The Wok has the best food","However, the food repeats for lunch and dinner"]
+    },
+    {
+      "name": "South Servery", 
+      "review": "8/10", 
+      "image": "https://wiess.rice.edu/images/new-students/commons.jpg",
+      "menu": "https://dining.rice.edu/south-servery",
+      "text": ["Best desserts","Wok station"]
+    }, 
+    {
+      "name": "West Servery", 
+      "review": "6/10", 
+      "image": "https://housing.rice.edu/sites/g/files/bxs3096/files/inline-images/HEWV_Rice_DuncanCommons_11.jpg",
+      "menu": "https://dining.rice.edu/west-servery",
+      "text": ["Stirfry Station",]
+    }, 
+    {
+      "name": "Seibel Servery", 
+      "review": "4/10", 
+      "image": "https://cdnassets.hw.net/dims4/GG/bfb60fa/2147483647/thumbnail/300x200%3E/quality/90/?url=https%3A%2F%2Fcdnassets.hw.net%2F50%2Fcf%2F06be80534e81b7aaf42214b26293%2F08017-00-n3.jpg",
+      "menu": "https://dining.rice.edu/seibel-servery",
+      "text": ["Stirfy station",]
+    }, 
+    
+    {
+      "name": "Baker Kitchen", 
+      "review": "5/10", 
+      "image": "https://i.ytimg.com/vi/gMO7iG6L7kE/maxresdefault.jpg",
+      "menu": "https://dining.rice.edu/baker-college-kitchen",
+      "text": ["Hit or miss"]
+    }]
+    
+  return (
+    <div className="App">
+      <h1 class="header">Servery Rankings</h1>
+      <div className="body">
+        {serveries.map(serveries => 
+          <Servery  name={serveries.name} image={serveries.image} review={serveries.review} menu={serveries.menu} text={serveries.text}/>
+        )}
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Servery.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "../Components.css"
+
+const Servery = (props) =>  {
+    return (
+        <div className="servery">
+            <h2>{props.name}</h2> 
+            <div>
+                <a target="_blank" href={props.menu}><img className="servery-image" src={props.image} alt={props.name}/></a>
+            </div>
+            <h3>Review: {props.review}</h3>
+            <div>
+                <ul>
+                {props.text.map(description => <li>{description}</li>)}
+                </ul>
+            </div>
+
+        </div> 
+    )
+}
+
+export default Servery
